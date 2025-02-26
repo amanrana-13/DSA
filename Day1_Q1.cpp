79d2c35a5e7846f314bd926e308091b2805764c9
AmanR <amanrana1317@gmail.com>
2025-02-26 17:09:14 +0530

    Add New File

---
+++ Day1_Q1.cpp
@@ -0,0 +1,25 @@
+// Question Link - https://leetcode.com/problems/ugly-number/description/?envType=problem-list-v2&envId=math
+// Question Name - Ugly Number
+class Solution {
+    public:
+        bool isUgly(int n) {
+            if (n < 1) {
+                return false;
+            }
+            while (n > 1) {
+                if (n % 2 == 0) {
+                    n /= 2;
+                }
+                else if (n % 3 == 0) {
+                    n /= 3;
+                }
+                else if (n % 5 == 0) {
+                    n /= 5;
+                } 
+                else {
+                    return false;
+                }
+            }
+            return true;
+        }
+    };
