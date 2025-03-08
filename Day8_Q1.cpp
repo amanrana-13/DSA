83e00b556a337c46f8f9342e03511c908e6cacdf
AmanR <amanrana1317@gmail.com>
2025-03-09 01:48:07 +0530

    Added new file

---
+++ Day8_Q1.cpp
@@ -0,0 +1,19 @@
+// Question Link - https://leetcode.com/problems/move-zeroes/description/
+// Question Name - Move Zeroes
+
+#include<iostream>
+using namespace std;
+#include<vector>
+class Solution {
+    public:
+        void moveZeroes(vector<int>& arr) {
+            int n = arr.size();
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = 0; j < n - 1 - i; j++) {
+                    if (arr[j] == 0) {
+                        swap(arr[j], arr[j + 1]);
+                    }
+                }
+            }
+        }
+    };
