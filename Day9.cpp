20564b8020bce01666494bc45c2fda034f6d85f6
AmanR <amanrana1317@gmail.com>
2025-03-11 01:20:10 +0530

    Added New Solutions

--- Day9.cpp
+++ Day9.cpp
@@ -1,10 +1,36 @@
-// Question 2
-// Question Link - https://leetcode.com/problems/sort-colors/description/
-// Question Name - Sort Colors
+// Question 1
+// Question Link - https://leetcode.com/problems/two-sum/description/
+// Question Name - Two Sum
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
+
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            int n = nums.size();
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        return {i, j};
+                    }
+                }
+            }
+            return {};
+        }
+    };
+
+// Time Complexity - O(n^2)
+// Space Complexity - O(1)
+
+
+
+// Question 2
+// Question Link - https://leetcode.com/problems/sort-colors/description/
+// Question Name - Sort Colors
+
 class Solution
 {
 public:
