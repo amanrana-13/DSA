3db33b7ff3e322e9bc98810ab0bf8a221b603b12
AmanR <amanrana1317@gmail.com>
2025-03-13 00:14:58 +0530

    Added New Solutions

--- Day11.cpp
+++ Day11.cpp
@@ -1,5 +1,25 @@
 // Question - 118 - Pascal Triangle
 // Question Link - https://leetcode.com/problems/pascals-triangle/description/
+#include<stdio.h>
+#include<vector>
+using namespace std;
+class Solution {
+    public:
+        vector<vector<int>> generate(int numRows) {
+            vector<vector<int>> ans;
+            if (numRows == 0) {
+                return ans;
+            }
+            for (int i = 0; i < numRows; ++i) {
+                vector<int> row(i + 1, 1);
+                for (int j = 1; j < i; ++j) {
+                    row[j] = ans[i - 1][j - 1] + ans[i - 1][j];
+                }
+                ans.push_back(row);
+            }
+            return ans;
+        }
+    };
 
 // Time Complexity - O()
 // Space Complexity - O()
