82466ea07ecc0c6dd8b55250a8b754a87b0636d4
AmanR <amanrana1317@gmail.com>
2025-03-13 23:04:43 +0530

    Added New Solution

--- Day11.cpp
+++ Day11.cpp
@@ -2,6 +2,7 @@
 // Question Link - https://leetcode.com/problems/pascals-triangle/description/
 #include<stdio.h>
 #include<vector>
+#include<algorithm>
 using namespace std;
 class Solution {
     public:
@@ -27,6 +28,23 @@ class Solution {
 
 // Question - 48 - Rotate Image 
 // Question Link - https://leetcode.com/problems/rotate-image/description/
+
+class Solution {
+    public:
+        void rotate(vector<vector<int>>& matrix) {
+            int n = matrix.size();
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    swap(matrix[i][j], matrix[j][i]);
+                }
+            }
+            for (int i = 0; i < n; i++) {
+                reverse(matrix[i].begin(),matrix[i].end());
+            }
+        }
+    };
+
+
 
 // Time Complexity - O()
 // Space Complexity - O()
