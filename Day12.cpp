3f30033138e169ef4ffead69241308f0a086b588
AmanR <amanrana1317@gmail.com>
2025-03-15 02:46:14 +0530

    Added New Solutions

--- Day12.cpp
+++ Day12.cpp
@@ -24,8 +24,34 @@ class Solution {
 // Question - 34 - Find First and Last Position of Element in Sorted Array
 // Question Link - https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
 
-// Time Complexity - O()
-// Space Complexity - O()
+class Solution {
+    public:
+        vector<int> searchRange(vector<int>& nums, int target) {
+            int n = nums.size();
+            if (n == 0) {
+                return {-1, -1};
+            }
+            int first = -1, last = -1;
+            for (int i = 0; i < n; i++) {
+                if (nums[i] == target) {
+                    first = i;
+                    break;
+                }
+            }
+            if (first != -1) {
+                for (int i = n - 1; i >= 0; i--) {
+                    if (nums[i] == target) {
+                        last = i;
+                        break;
+                    }
+                }
+            }
+            return {first, last};
+        }
+    };
+
+// Time Complexity - O(n)
+// Space Complexity - O(1)
 
 
 // Question - 35 - Search Insert Position
