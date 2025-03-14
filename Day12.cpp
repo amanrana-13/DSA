5bda7be759e11e1c3bac8950f86b6aa58a8c30e6
AmanR <amanrana1317@gmail.com>
2025-03-14 14:54:46 +0530

    Added New Solution

---
+++ Day12.cpp
@@ -0,0 +1,12 @@
+// Question - 34 - Find First and Last Position of Element in Sorted Array
+// Question Link - https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+
+// Time Complexity - O()
+// Space Complexity - O()
+
+
+// Question - 35 - Search Insert Position
+// Question Linl - https://leetcode.com/problems/search-insert-position/description/
+
+// Time Complexity - O()
+// Space Complexity - O()
