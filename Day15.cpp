2b9dff6a6e509396188ee974153820297ce2e591
AmanR <amanrana1317@gmail.com>
2025-03-18 00:59:34 +0530

    Added New Solution

--- Day15.cpp
+++ Day15.cpp
@@ -22,3 +22,25 @@ class Solution {
 
 // Time Complexity - O(m * n)
 // Space Complexity - O(1)
+
+// Question - 240. Search a 2D Matrix II
+// Question Link - https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+
+class Solution {
+    public:
+        bool searchMatrix(vector<vector<int>>& matrix, int target) {
+            int m = matrix.size();
+            int n = matrix[0].size();
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (target == matrix[i][j]) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    };
+
+// Time Complexity - O(m*n)
+// Space Complexity - O(1)
