dde47868a21a313cdc24b0ab68c78a5fcdfb1924
AmanR <amanrana1317@gmail.com>
2025-03-18 17:49:29 +0530

    Added New Solutions

--- Day16.cpp
+++ Day16.cpp
@@ -3,8 +3,50 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+class Solution {
+    public:
+        int findMaxIndex(vector<vector<int>>& mat, int m, int n, int col) {
+            int MaxValue = -1;
+            int Index = -1;
+            for (int i = 0; i < m; i++) {
+                if (mat[i][col] > MaxValue) {
+                    MaxValue = mat[i][col];
+                    Index = i;
+                }
+            }
+            return Index;
+        }
+        vector<int> findPeakGrid(vector<vector<int>>& mat) {
+            int m = mat.size();
+            int n = mat[0].size();
+            int low = 0;
+            int high = n - 1;
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                int maxRowIndex = findMaxIndex(mat, m, n, mid);
+                int left = mid - 1 >= 0 ? mat[maxRowIndex][mid - 1] : -1;
+                int right = mid + 1 < n ? mat[maxRowIndex][mid + 1] : -1;
+                int top = maxRowIndex - 1 >= 0 ? mat[maxRowIndex - 1][mid] : -1;
+                int bottom = maxRowIndex + 1 < m ? mat[maxRowIndex + 1][mid] : -1;
+    
+                if (mat[maxRowIndex][mid] > left && mat[maxRowIndex][mid] > right &&
+                    mat[maxRowIndex][mid] > top && mat[maxRowIndex][mid] > bottom) {
+                    return {maxRowIndex, mid};
+                } else if (mat[maxRowIndex][mid] < left) {
+                    high = mid - 1;
+                } else if (mat[maxRowIndex][mid] < right) {
+                    low = mid + 1;
+                } else if (mat[maxRowIndex][mid] < top) {
+                    maxRowIndex = maxRowIndex - 1;
+                } else {
+                    maxRowIndex = maxRowIndex + 1;
+                }
+            }
+            return {-1, -1};
+        }
+    };
 
-// Time Complexity - O()
+// Time Complexity - O(m log n )
 // Space Complexity - O(1)
 
 // // Question - Median in a row-wise sorted Matrix
